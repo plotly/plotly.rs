7efd1a9419ad61663adb71c73fbf9685c2403b0f
Michael Freeborn <michaelfreeborn1@gmail.com>
2022-04-22 17:57:06 +0100

    add plotly_white and default (empty) themes

--- plotly/src/themes.rs
+++ plotly/src/themes.rs
@@ -5,6 +5,87 @@ use crate::{
     layout::{Axis, ColorAxis, HoverMode, LayoutColorScale, LayoutTemplate, Template},
 };
 
+pub static DEFAULT: Lazy<Template> = Lazy::new(|| {
+    let layout_template = LayoutTemplate::new();
+    let template = Template::new().layout(layout_template);
+    template
+});
+
+pub static PLOTLY_WHITE: Lazy<Template> = Lazy::new(|| {
+    let layout_template = LayoutTemplate::new()
+        .color_axis(ColorAxis::new().color_bar(ColorBar::new().outline_width(0)))
+        .color_scale(
+            LayoutColorScale::new()
+                .sequential(ColorScale::Vector(vec![
+                    ColorScaleElement(0., "#0d0887".to_string()),
+                    ColorScaleElement(0.1111111111111111, "#46039f".to_string()),
+                    ColorScaleElement(0.2222222222222222, "#7201a8".to_string()),
+                    ColorScaleElement(0.3333333333333333, "#9c179e".to_string()),
+                    ColorScaleElement(0.4444444444444444, "#bd3786".to_string()),
+                    ColorScaleElement(0.5555555555555556, "#d8576b".to_string()),
+                    ColorScaleElement(0.6666666666666666, "#ed7953".to_string()),
+                    ColorScaleElement(0.7777777777777778, "#fb9f3a".to_string()),
+                    ColorScaleElement(0.8888888888888888, "#fdca26".to_string()),
+                    ColorScaleElement(1., "#f0f921".to_string()),
+                ]))
+                .sequential_minus(ColorScale::Vector(vec![
+                    ColorScaleElement(0., "#0d0887".to_string()),
+                    ColorScaleElement(0.1111111111111111, "#46039f".to_string()),
+                    ColorScaleElement(0.2222222222222222, "#7201a8".to_string()),
+                    ColorScaleElement(0.3333333333333333, "#9c179e".to_string()),
+                    ColorScaleElement(0.4444444444444444, "#bd3786".to_string()),
+                    ColorScaleElement(0.5555555555555556, "#d8576b".to_string()),
+                    ColorScaleElement(0.6666666666666666, "#ed7953".to_string()),
+                    ColorScaleElement(0.7777777777777778, "#fb9f3a".to_string()),
+                    ColorScaleElement(0.8888888888888888, "#fdca26".to_string()),
+                    ColorScaleElement(1., "#f0f921".to_string()),
+                ]))
+                .diverging(ColorScale::Vector(vec![
+                    ColorScaleElement(0., "#8e0152".to_string()),
+                    ColorScaleElement(0.1, "#c51b7d".to_string()),
+                    ColorScaleElement(0.2, "#de77ae".to_string()),
+                    ColorScaleElement(0.3, "#f1b6da".to_string()),
+                    ColorScaleElement(0.4, "#fde0ef".to_string()),
+                    ColorScaleElement(0.5, "#f7f7f7".to_string()),
+                    ColorScaleElement(0.6, "#e6f5d0".to_string()),
+                    ColorScaleElement(0.7, "#b8e186".to_string()),
+                    ColorScaleElement(0.8, "#7fbc41".to_string()),
+                    ColorScaleElement(0.9, "#4d9221".to_string()),
+                    ColorScaleElement(1., "#276419".to_string()),
+                ])),
+        )
+        .colorway(vec![
+            "#636efa", "#EF553B", "#00cc96", "#ab63fa", "#FFA15A", "#19d3f3", "#FF6692", "#B6E880",
+            "#FF97FF", "#FECB52",
+        ])
+        .font(Font::new().color("#2a3f5f"))
+        .hover_label(Label::new().align("left"))
+        .hover_mode(HoverMode::Closest)
+        .paper_background_color("#ffffff")
+        .plot_background_color("#ffffff")
+        .title(Title::new("").x(0.05))
+        .x_axis(
+            Axis::new()
+                .auto_margin(true)
+                .grid_color("#EBF0F8")
+                .line_color("#EBF0F8")
+                // missing title.standoff = 15
+                .zero_line_color("#EBF0F8")
+                .zero_line_width(2),
+        )
+        .y_axis(
+            Axis::new()
+                .auto_margin(true)
+                .grid_color("#EBF0F8")
+                .line_color("#EBF0F8")
+                // missing title.standoff = 15
+                .zero_line_color("#EBF0F8")
+                .zero_line_width(2),
+        );
+    let template = Template::new().layout(layout_template);
+    template
+});
+
 pub static PLOTLY_DARK: Lazy<Template> = Lazy::new(|| {
     // the following are unimplemented: layout.autotypenumbers, layout.polar, layout.ternary,
     // layout.scene, layout.geo, layout.mapbox, layout.*defaults
