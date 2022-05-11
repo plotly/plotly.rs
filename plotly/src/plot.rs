de013e9c62582ef333447e9aa00679de58ac7846
Michael Freeborn <31806808+mfreeborn@users.noreply.github.com>
2022-05-11 13:41:42 +0100

    Update plotly js (#80)
    * update plotly and mathjax deps
    
    * fix imports + typo
    
    * update changelog

--- plotly/src/plot.rs
+++ plotly/src/plot.rs
@@ -9,15 +9,13 @@ use serde::Serialize;
 use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use crate::{Configuration, Layout};
 use rand_distr::Alphanumeric;
 
-const PLOTLY_JS: &str = "plotly-2.8.3.min.js";
-
 #[derive(Template)]
-#[template(path = "plotly-2.8.3.min.js", escape = "none")]
+#[template(path = "plotly.min.js", escape = "none")]
 struct PlotlyJs;
 
 #[derive(Template)]
@@ -430,12 +428,6 @@ impl Plot {
             .unwrap_or_else(|_| panic!("failed to export plot to {:?}", filename.as_ref()));
     }
 
-    fn plotly_js_path() -> PathBuf {
-        let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let templates = root.join("templates");
-        templates.join(PLOTLY_JS)
-    }
-
     fn render(
         &self,
         export_image: bool,
@@ -477,7 +469,7 @@ impl Plot {
         js_sys::JSON::parse(&self.to_json())
             .expect("Invalid JSON")
             .dyn_into::<js_sys::Object>()
-            .expect("Invalid JSON structure - expected an top-level Object")
+            .expect("Invalid JSON structure - expected a top-level Object")
     }
 
     #[cfg(target_os = "linux")]
@@ -518,6 +510,7 @@ impl PartialEq for Plot {
 #[cfg(test)]
 mod tests {
     use serde_json::{json, to_value};
+    use std::path::PathBuf;
 
     use super::*;
     use crate::Scatter;
